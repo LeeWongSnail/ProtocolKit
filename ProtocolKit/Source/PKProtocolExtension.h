a749a2ea889b5f349fddc0fa59496eb1937406a9
LeeWongSnail <wangli_0632@163.com>
2018-03-30 11:41:44 +0800

    add Note

--- ProtocolKit/Source/PKProtocolExtension.h
+++ ProtocolKit/Source/PKProtocolExtension.h
@@ -24,12 +24,14 @@
 #import <objc/runtime.h>
 
 // For a magic reserved keyword color, use @defs(your_protocol_name)
+//defs 只是 _pk_extension 的别名，为了提供一个更加合适的名字作为接口 使用的时候用@defs()
 #define defs _pk_extension
 
-// Interface
+// 我们使用的时候用@defs(Protocol)实际上是调用_pk_extension_imp方法
 #define _pk_extension($protocol) _pk_extension_imp($protocol, _pk_get_container_class($protocol))
 
-// Implementation
+// 这个宏实际上是创建了一个隐藏类(container_class) 这个类遵守protocol这个协议
+// 然后在load方法中调用内部实现的load(load方法也是在main函数执行之前执行的)
 #define _pk_extension_imp($protocol, $container_class) \
     protocol $protocol; \
     @interface $container_class : NSObject <$protocol> @end \
@@ -38,7 +40,8 @@
         _pk_extension_load(@protocol($protocol), $container_class.class); \
     } \
 
-// Get container class name by counter
+// _pk_get_container_class 实际上就是生成一个类名
+// 可以看到这个类名实际上是由雷鸣+协议名+count(一个计数器每次调用会自动加一)
 #define _pk_get_container_class($protocol) _pk_get_container_class_imp($protocol, __COUNTER__)
 #define _pk_get_container_class_imp($protocol, $counter) _pk_get_container_class_imp_concat(__PKContainer_, $protocol, $counter)
 #define _pk_get_container_class_imp_concat($a, $b, $c) $a ## $b ## _ ## $c
